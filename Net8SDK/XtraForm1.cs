3048792a71e8023529c4ed7fe5e819a59dc7380e
Jon Shadforth <jon@carpediemsystems.co.uk>
2025-03-12 07:23:14 +0000

    Added environment info to the caption bars

--- Net8SDK/XtraForm1.cs
+++ Net8SDK/XtraForm1.cs
@@ -1,21 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Text;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using DevExpress.XtraEditors;
+﻿using System.Runtime.InteropServices;
 
 namespace Net8SDK
 {
-	public partial class XtraForm1: DevExpress.XtraEditors.XtraForm
-	{
+    public partial class XtraForm1 : DevExpress.XtraEditors.XtraForm
+    {
         public XtraForm1()
-		{
+        {
             InitializeComponent();
-		}
-	}
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            string appName = Application.ProductName!;
+            string appVersion = Application.ProductVersion.Split('+')[0]; // Remove hash if present
+
+            string appBitDepth = Environment.Is64BitProcess ? "64-bit" : "32-bit";
+            string appArchitecture = RuntimeInformation.ProcessArchitecture.ToString();
+            string appFramework = RuntimeInformation.FrameworkDescription;
+
+            string osVersion = Environment.OSVersion.VersionString;
+            string osBitDepth = Environment.Is64BitOperatingSystem ? "64-bit" : "32-bit";
+            string osArchitecture = RuntimeInformation.OSArchitecture.ToString();
+
+            Text =
+                $"Application: {appName} [{appVersion}] " +
+                $"running as {appBitDepth} {appArchitecture} " +
+                $"using {appFramework} " +
+                $"on {osVersion} {osBitDepth} and {osArchitecture} processor";
+
+        }
+
+    }
 }
